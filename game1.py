2e388a1e9db4fad1fcdde1e7dd06427ab3bdb41b
stefan <dev@2904.cc>
2013-11-03 00:07:04 +0100

    added speed as program argument

--- game1.py
+++ game1.py
@@ -1,20 +1,37 @@
+import sys
 import os
 import time
 from random import randint
 
 # Config:
-speed 		= 0.2 # call a command every 'speed' seconds
+speed 		= 0.4 # call a command every 'speed' seconds
 commands 	= ["red", "blue", "purple"]
 
 # Global vars:
 counter = 0
 
 def say(word):
-	nr = randint(1,len(commands)*10) % len(commands)
-	os.system("say " +commands[nr])
+	print word
+	os.system("say " +word)
 
 def say_random_command():
-	say("red")
+	nr = randint(1,len(commands)*10) % len(commands)
+	say(commands[nr])
+
+def handle_args():
+	prog = sys.argv[0]
+	global speed
+	if len(sys.argv) < 2:
+		print "no speed argument found, asuming speed=", speed
+		print "to set speed, start program like this:"
+		print prog, "<speed>"
+	else:
+		speed = float(sys.argv[1])
+		print "running", prog, "with speed=", speed
+
+# main:
+
+handle_args()
 
 try:
 	while True:
@@ -23,4 +40,5 @@ try:
 		time.sleep(speed)
 except KeyboardInterrupt:
 			time.sleep(0.5)
-			say("So we are done, you played " +str(counter) +" rounds! bye bye")
+			
+say("So we are done, you played " +str(counter) +" rounds! bye bye")
